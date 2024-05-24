b93716b0eea76b008ce27b67e2a55a3aaf2f75b0
Gouvea <caua.nascimento@sptech.school>
2024-05-23 23:24:44 -0300

    Adição do post ao feed, estilização do feed

---
+++ public/js/newPost.js
@@ -0,0 +1,28 @@
+`<div class="feedPost">
+<div class="postUserInfo">
+    <img src="./assets/img/pfpIcon.svg" class="iconPost">
+    <div class="postUserText">
+        <span class="postUserNome">usuario</span>
+        <span class="postUserHora">4h atras</span>
+    </div>
+</div>
+<div id="postContentContainer">
+    <h2 id="tituloPost">Alguém me ajuda pelo amor de deus</h2>
+    <span class="postTexto">No genshin que eu não to conseguindo passar nem com reza brabaNo genshin que eu não to conseguindo passar nem com reza brabaNo genshin que eu não to conseguindo passar nem com reza braba</span>
+    <div class="partyList">
+        <img src="./assets/img/pfpIcon.svg" class="iconPost">
+        <img src="./assets/img/pfpIcon.svg" class="iconPost">
+        <img src="./assets/img/pfpIcon.svg" class="iconPost">
+        <img src="./assets/img/plusIcon.svg" class="iconPost">
+        <img src="./assets/img/plusIcon.svg" class="iconPost">
+    </div>
+</div>
+<div class="postInteracao">
+    <div class="interacao" id="divCurtida">
+        <i class="fa-regular fa-heart postInteracaoIcon" id="iconCurtida"></i> 300
+    </div>
+    <div class="interacao" id="divComent">
+        <i class="fa-regular fa-comments postInteracaoIcon" id="iconComent"></i> 227
+    </div>
+</div>
+</div>`
