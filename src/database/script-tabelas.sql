dbd4a228caf88033126d17eeffa4f9d7352cec2f
Gouvea <caua.nascimento@sptech.school>
2024-05-23 23:27:15 -0300

    Mudança na modelagem do banco

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -9,8 +9,35 @@ comandos para mysql server
 create database zephyr;
 use zephyr;
 
+create table imagem(
+	idImagem int primary key auto_increment,
+    imagem MEDIUMBLOB);
+
 create table usuario(
 	idUsuario int primary key auto_increment,
-    usuario varchar(45),
-    email varchar(255),
-    senha varchar(45));
+    usuario varchar(45) unique,
+    email varchar(255) unique,
+    senha varchar(45),
+    fkImagem int,
+    foreign key (fkImagem) references imagem(idImagem));
+
+create table post (
+	idPost int auto_increment,
+    fkUsuario int,
+    foreign key (fkUsuario) references usuario(idUsuario),
+    titulo varchar(50),
+    descricao varchar(250),
+    dataHora datetime,
+    curtida int,
+    fkImagem int,
+    foreign key (fkImagem) references imagem(idImagem));
+    
+create table comentarioPost (
+	idComentario int,
+    fkPost int,
+    fkUsuario int,
+    primary key	(idComentario, fkPost, fkUsuario),
+    foreign key (fkPost) references post(idPost),
+    foreign key (fkUsuario) references usuario(idUsuario),
+    mensagem varchar(250),
+    dataHora datetime);
